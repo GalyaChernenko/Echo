e318c4931df2d5cc636af0fd727947109566049a
Chernenko <galka8@inbox.ru>
2023-04-14 12:25:40 +0300

    Initial commit

---
+++ src/test/java/ru/netology/echo/EchoTest.java
@@ -0,0 +1,24 @@
+package ru.netology.echo;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+class EchoTest {
+    @Test
+
+    void shouldReturnEcho() {
+        given()
+                .baseUri("https://postman-echo.com")
+                .contentType("text/plain; charset=UTF-8")
+                .body("Hi")
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("Hh"))
+        ;
+
+    }
+}
